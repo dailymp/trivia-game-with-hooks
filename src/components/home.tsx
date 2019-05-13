679d622d34c1da547fde29955bbdd5427d3b8396
Alvaro Perez <apgarcia@ingenia.es>
2019-05-13 18:24:48 +0300

    Refactoring using hooks

---
+++ src/components/home.tsx
@@ -0,0 +1,107 @@
+import * as React from 'react';
+import { Card, CardHeader, CardContent, WithStyles, createStyles, withStyles, Button, ButtonBase, StepButton } from "@material-ui/core";
+import { Result } from '../model/questionsResults';
+import { ResultsContainer } from './resultsContainer';
+import { QuestionsContainerComponent } from './questionsContainer';
+
+const styles = theme =>
+  createStyles({
+    card: {
+      maxWidth: 400,
+      margin: "0 auto",
+      alignContent: 'center'
+    },
+    cardHeader: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignContent: 'center',
+      padding: 30,
+      marginBottom: 60,
+      fontSize: 20,
+      fontWeight: 'bold',
+      fontFamily: 'Arial'
+    },
+    button: {
+marginBottom: 20,
+    }
+  });
+
+  const resultFinish: Result = {
+    results: [
+      {
+        category: "",
+        type: "",
+        difficulty: "",
+        question: "aaaaaaaa",
+        correct_answer: "True",
+        incorrect_answers: [""],
+        user_answer: true
+      },
+      {
+        category: "",
+        type: "",
+        difficulty: "",
+        question: "bbbbbb",
+        correct_answer: "True",
+        incorrect_answers: [""],
+        user_answer: false
+      }
+    ],
+    response_code: 1
+  }
+
+interface HomeProps extends WithStyles<typeof styles> {}
+
+const HomeComponent = (props: HomeProps) => {
+    const [begin, setBegin] = React.useState(false);
+    const [result, setResult] = React.useState(resultFinish);
+
+    const finishGame = (result: Result) => {
+        setBegin(false);
+        setResult(result);
+    }
+
+    const resetGame = () => {
+        setBegin(true);
+        setResult(null);
+    }
+
+    const renderQuestions = () => {
+        return (
+            <React.Fragment>
+              <div   className={classes.cardHeader} >
+                <div className={classes.cardHeader}>You will be presented with 10 true or false questions</div>
+                <div className={classes.cardHeader}>Can you score 100%?</div>
+
+                <div  className={classes.card}>
+                <Button onClick= { () => this.setState({ begin: true})}
+                  variant="contained" color="secondary" className={classes.button}>Begin</Button>
+                </div>
+              </div>
+            { begin && <QuestionsContainerComponent setFinalResult={finishGame}/> }
+            </React.Fragment>
+          )
+    }
+
+    const renderFinalResult = () => {
+        return (
+            <ResultsContainer
+              result={result}
+              resetGame={resetGame}
+            />
+          )
+    }
+
+    //Even it's referenced above, we can define it here due to JS hoisting
+    const { classes } = props;
+    return (
+        <Card className={classes.card}>
+          {!result && <CardHeader className= {classes.cardHeader}  title="Welcome to the trivia challenge" />}
+          <CardContent>
+          { result ? renderFinalResult() : renderQuestions() }
+          </CardContent>
+        </Card>
+    )
+}
+
+export const Home = withStyles(styles)(HomeComponent);
--- src/components/resultsContainer.tsx
+++ src/components/resultsContainer.tsx
@@ -1,7 +1,18 @@
 import * as React from "react";
 import { Result, Questions } from "../model/questionsResults";
+import { createStyles, WithStyles, withStyles } from "@material-ui/core";
+import DoneIcon from '@material-ui/icons/Done';
+import ClearIcon from '@material-ui/icons/Clear';
+const centeredElement = {"display": "flex", "align-items": "center", "justify-content": "center", "flex-direction": "column"};
 
-interface resultsContainerProps {
+const styles = theme =>
+  createStyles({
+    Score: centeredElement,
+    PlayAgain: {...centeredElement, "marginTop": "1rem"},
+    Result: {"display": "flex", "align-items": "center", "justify-content": "left", "border":"1px solid black", "padding": "0.5rem"}
+  });
+
+interface resultsContainerProps extends WithStyles<typeof styles> {
     result: Result;
     resetGame: () => void;
 }
@@ -10,32 +21,32 @@ const checkIfAnswerIsCorrect = (question: Questions): boolean => {
     return question.correct_answer.toLocaleLowerCase() === question.user_answer.toString();
 }
 
-const centeredElements = {"display": "flex", "align-items": "center", "justify-content": "center"};
-
-export const ResultsContainer = (props: resultsContainerProps) => {
-    const { result, resetGame } = props;
+const ResultsContainerComponent = (props: resultsContainerProps) => {
+    const { result, resetGame, classes } = props;
     const getScore = () => {
         return result.results.reduce((i,c,f) => checkIfAnswerIsCorrect(c) ? f++ : f, 0);
     }
     return (
         <React.Fragment>
-            <div style={centeredElements}>
-            <h3>You scored</h3>
-            <h3>{`${getScore()}/${result.results.length}`}</h3>
+            <div className={classes.Score}>
+            <h3>You scored </h3>
+            <h3>{` ${getScore()}/${result.results.length} `}</h3>
             </div>
             {
                 result.results.map(question => {
                     return (
-                        <div style={{"display": "flex", "align-items": "center", "justify-content": "left"}}>
-                            <span style={{"padding": 10}}>{checkIfAnswerIsCorrect(question) ? "+" : "-"}</span>
+                        <div className={classes.Result}>
+                            <span style={{"padding": 10}}>{checkIfAnswerIsCorrect(question) ? <DoneIcon/> : <ClearIcon/>}</span>
                             <p>{question.question}</p>
                         </div>
                     )
                 })
             }
-            {<div style={centeredElements}>
+            {<div className={classes.PlayAgain}>
             <button onClick={() => resetGame()}>Play Again?</button>
             </div>}
         </React.Fragment>
     );
 };
+
+export const ResultsContainer = withStyles(styles)(ResultsContainerComponent);
