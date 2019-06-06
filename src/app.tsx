39f1cd9c2d054b2b280208024c75cfc21fa2c567
Juan Francisco Chico Moreno <JuanF@MBP-de-Juan-2.home>
2019-06-06 21:25:35 +0200

    rebase done

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { QuestionsContainerComponent } from './components/questionsContainer';
 import { Card, CardHeader, CardContent, WithStyles, createStyles, withStyles, Button, ButtonBase, StepButton } from "@material-ui/core";
+import { ResultsContainer } from './components/resultsContainer';
+import { Result } from './model/questionsResults';
 
 
 const styles = theme =>
@@ -26,24 +28,79 @@ const styles = theme =>
   });
 
 interface State {
-  begin: boolean
+  begin: boolean;
+  result: Result;
 }
 
 interface Props extends WithStyles<typeof styles> { }
 
-export class AppInner extends React.Component<Props, State> {
+const resultFinish: Result = {
+  results: [
+    {
+      category: "",
+      type: "",
+      difficulty: "",
+      question: "aaaaaaaa",
+      correct_answer: "True",
+      incorrect_answers: [""],
+      user_answer: true
+    },
+    {
+      category: "",
+      type: "",
+      difficulty: "",
+      question: "bbbbbb",
+      correct_answer: "True",
+      incorrect_answers: [""],
+      user_answer: false
+    }
+  ],
+  response_code: 1
+}
 
+export class AppInner extends React.Component<Props, State> {
   /**
    *
    */
   constructor(props: Props) {
     super(props);
-    this.state = { begin: false }
+    this.state = { begin: false, result: null }
+    this.finishGame = this.finishGame.bind(this);
+    this.resetGame = this.resetGame.bind(this);
+  }
+
+  public finishGame(result: Result) {
+    this.setState({ result, begin: false });
+  }
+
+  public resetGame() {
+    this.setState({ result: null, begin: true});
+  }
+
+  public renderHome() {
+    const { classes } = this.props;
+    const { begin, result } = this.state;
+    
+    return (
+      <React.Fragment>
+      {!begin ?
+        <div   className={classes.cardHeader} >
+          <div className={classes.cardHeader}>You will be presented with 10 true or false questions</div>
+          <div className={classes.cardHeader}>Can you score 100%?</div>
+
+          <div  className={classes.card}>
+          <Button onClick= { () => this.setState({ begin: true})}
+            variant="contained" color="secondary" className={classes.button}>Begin</Button>
+          </div>
+        </div>
+      : <QuestionsContainerComponent setFinalResult={this.finishGame}/> }
+      </React.Fragment>
+    )
   }
 
   public render() {
     const { classes } = this.props;
-    const { begin } = this.state;
+    const { begin, result } = this.state;
 
     return (
       <>
@@ -51,18 +108,15 @@ export class AppInner extends React.Component<Props, State> {
           {!begin && <CardHeader className={classes.cardHeader} title="Welcome to the trivia challenge" /> }
           
           <CardContent>
-            {!begin && 
-              <div className={classes.cardHeader} >
-                <div className={classes.cardHeader}>You will be presented with 10 true or false questions</div>
-                <div className={classes.cardHeader}>Can you score 100%?</div>
-                <div className={classes.card}>
-                  <Button onClick={() => this.setState({ begin: true })}
-                    variant="contained" color="secondary" className={classes.button}>Begin</Button>
-                </div>
-              </div>
+            {result ?
+              <ResultsContainer
+                result={result}
+                resetGame={this.resetGame}
+              />
+              :
+              this.renderHome()
             }
 
-            {begin && <QuestionsContainerComponent />}
           </CardContent>
         </Card>
       </>
--- src/components/questionsContainer.tsx
+++ src/components/questionsContainer.tsx
@@ -1,22 +1,48 @@
 import * as React from "react";
 import { questionsApi } from "../api/questionsAPI";
 import { QuestionsItem } from "./questionsItem";
+import { QuestionBox } from "./questionBox";
+import { Result, Questions } from "../model/questionsResults";
 
 function useQuestions() {
   const [questions, setQuestions] = React.useState([]);
-
   const loadQuestions = () => {
     questionsApi.getAllQuestions().then(questions => setQuestions(questions));
   };
-  return {  questions,  loadQuestions };
+
+  return {  questions, loadQuestions, setQuestions };
 }
 
-export const QuestionsContainerComponent = () => {
-  const { questions, loadQuestions } = useQuestions();
+interface QuestionsContainerProps {
+  setFinalResult: (results: Result) => void;
+}
+
+export const QuestionsContainerComponent = (props: QuestionsContainerProps) => {
+  const { setFinalResult } = props;
+  const { questions, loadQuestions, setQuestions } = useQuestions();
 
   React.useEffect(() => {
     loadQuestions();
   }, []);
+
+  const addAnswer = (index: number, answer: boolean) => {
+    //Update answer questions using Immutable way
+    const questionsUpdated = {
+      ...questions.slice(0, index), // questions before current update
+      ...questions[index], user_answer: answer,
+      ...questions.slice(index + 1) // questions after current update
+    }
+    setQuestions(questionsUpdated);
+  }
+
+  //When game is finished
+  const finishGame = () => {
+    const result: Result = {
+      response_code: Math.random(),
+      results: questions,
+    }
+    setFinalResult(result);
+  }
 
   return (
     // As a first approach, a new component is created in order to manage the state in the component without having to use Hooks
